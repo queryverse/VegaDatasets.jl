bde50a59e5d06b3f1f0ae3b1d658f384517a1112
David Anthoff <anthoff@berkeley.edu>
2018-03-02 16:41:05 -0800

    Add initial version

--- src/VegaDatasets.jl
+++ src/VegaDatasets.jl
@@ -1,5 +1,38 @@
 module VegaDatasets
 
-# package code goes here
+using DataFrames, JSON, TextParse
+
+export dataset
+
+function dataset(name::AbstractString)
+    json_filename = joinpath(@__DIR__,"..","data", "data", "$name.json")
+    csv_filename = joinpath(@__DIR__,"..","data","data", "$name.csv")
+    if isfile(json_filename)
+        json_data = JSON.parsefile(json_filename)
+        colnames = collect(Symbol(i) for i in keys(json_data[1]))
+
+        coltypes = Type[]
+        for col in colnames
+            col_type = typeof(json_data[1][String(col)])
+            for row in 2:length(json_data)
+                col_type = Base.promote_type(col_type, typeof(json_data[row][String(col)]))
+            end
+            push!(coltypes, col_type)
+        end
+
+        df = DataFrame(coltypes, colnames, 0)
+
+        for row in json_data
+            push!(df, ( [row[String(col)] for col in colnames]... ))
+        end
+        return df
+    elseif isfile(csv_filename)
+        data, header = csvread(csv_filename)
+
+        return DataFrame([data...], [Symbol(i) for i in header])
+    else
+        error("Unknown dataset.")
+    end
+end
 
 end # module
