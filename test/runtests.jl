3b0f433ab32525a01d9415839c552dc5e596e2ae
David Anthoff <anthoff@berkeley.edu>
2018-05-31 13:58:19 -0700

    Migrate to TableTraits.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,24 @@
 using VegaDatasets
-using DataFrames
 using Base.Test
 
 @testset "VegaDatasets" begin
 
 df = dataset("iris")
 
-@test isa(df, DataFrame)
+@test isa(df, VegaDatasets.VegaDataset)
 
 df2 = dataset("airports")
 
-@test isa(df2, DataFrame)
+@test isa(df2, VegaDatasets.VegaDataset)
 
 @test_throws ErrorException dataset("doesnotexist")
 
 df3 = dataset("weather.csv")
 
-@test isa(df3, DataFrame)
+@test isa(df3, VegaDatasets.VegaDataset)
 
 df4 = dataset("weather.json")
 
-@test isa(df4, DataFrame)
+@test isa(df4, VegaDatasets.VegaDataset)
 
 end
