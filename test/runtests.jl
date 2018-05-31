6b8c518f9c66371af8cfeaa16fe9791af1a8dd19
David Anthoff <anthoff@berkeley.edu>
2018-05-31 15:50:44 -0700

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,18 @@ using Base.Test
 
 @testset "VegaDatasets" begin
 
+for filename in readdir(joinpath(@__DIR__,"..","data","data"))
+    if splitext(filename)[2]==".json" || splitext(filename)[2]==".csv"
+        if !in(splitdir(filename)[2], ["earthquakes.json", "graticule.json", "londonBoroughs.json", "londonTubeLines.json", "miserables.json", "sf-temps.csv", "us-10m.json", "world-110m.json"])
+            vd = dataset(filename)
+
+            cvd = collect(vd)
+
+            @test isa(cvd, Array)
+        end
+    end
+end
+
 df = dataset("iris")
 
 @test isa(df, VegaDatasets.VegaDataset)
