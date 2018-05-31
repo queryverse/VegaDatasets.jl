37a2131b205a1399c942f1cabbad9622767bc9cf
David Anthoff <anthoff@berkeley.edu>
2018-05-31 16:05:50 -0700

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using VegaDatasets
+import IteratorInterfaceExtensions, TableTraits, TableShowUtils
 using Base.Test
 
 @testset "VegaDatasets" begin
@@ -7,6 +8,9 @@ for filename in readdir(joinpath(@__DIR__,"..","data","data"))
     if splitext(filename)[2]==".json" || splitext(filename)[2]==".csv"
         if !in(splitdir(filename)[2], ["earthquakes.json", "graticule.json", "londonBoroughs.json", "londonTubeLines.json", "miserables.json", "sf-temps.csv", "us-10m.json", "world-110m.json"])
             vd = dataset(filename)
+
+            @test IteratorInterfaceExtensions.isiterable(vd) == true
+            @test TableTraits.isiterabletable(vd) == true
 
             cvd = collect(vd)
 
@@ -25,6 +29,8 @@ df2 = dataset("airports")
 
 @test_throws ErrorException dataset("doesnotexist")
 
+@test_throws ErrorException dataset("7zip.png")
+
 df3 = dataset("weather.csv")
 
 @test isa(df3, VegaDatasets.VegaDataset)
@@ -32,5 +38,19 @@ df3 = dataset("weather.csv")
 df4 = dataset("weather.json")
 
 @test isa(df4, VegaDatasets.VegaDataset)
+
+@test sprint(show, dataset("lookup_groups")) == """
+9x2 Vega dataset
+group │ person
+──────┼───────
+1     │ Alan  
+1     │ George
+1     │ Fred  
+2     │ Steve 
+2     │ Nick  
+2     │ Will  
+3     │ Cole  
+3     │ Rick  
+3     │ Tom   """
 
 end
