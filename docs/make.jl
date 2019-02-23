61b34c12be6ba8c91e56f2cc4eaee343051189ab
David Anthoff <anthoff@berkeley.edu>
2019-02-23 12:58:21 -0800

    Add docs template

---
+++ docs/make.jl
@@ -0,0 +1,14 @@
+using Documenter, VegaDatasets
+
+makedocs(
+	modules = [VegaDatasets],
+	sitename = "VegaDatasets.jl",
+	analytics="UA-132838790-1",
+	pages = [
+        "Introduction" => "index.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/queryverse/VegaDatasets.jl.git"
+)
